d27255f4589e2bbf51a0a6683247e5eadc49fda3
Zach Mitchell <zach@ifixit.com>
2020-09-29 09:12:22 -0700

    Style: run Prettier on `src/example/app.js`

--- src/example/app.js
+++ src/example/app.js
@@ -23,91 +23,63 @@ const SpacedRecommendedProducts = styled.div`
 `;
 
 function App() {
-  const reviewsLink = '/User/Reviews';
+   const reviewsLink = '/User/Reviews';
 
-  return (
-    <div className="App">
-      <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet"/>
-      <ProductNote
-        type="note"
-        title="Example Note"
-        html="Example Contents"
-      />
-      <ProductNote
-        type="disclaimer"
-        title="Example Disclaimer"
-        html="Example Contents"
-      />
-      <ProductNote
-        type="warning"
-        title="Example Warning"
-        html="Example Contents"
-      />
-      <hr/>
-      <ReviewStars
-        numReviews={30}
-        average={3.2}
-        starSize={18}
-      />
-      <hr/>
-      <ProductReviews
-        productReviews={exampleReviewsNone}
-        langid="en"
-        itemcode="IF145-002-4"
-        reviewsLink={reviewsLink}
-      />
-      <hr/>
-      <ProductReviews
-        productReviews={exampleReviews}
-        langid="en"
-        itemcode="IF145-002-4"
-        reviewsLink={reviewsLink}
-      />
-      <hr/>
-      <RecommenededProductsExample/>
-    </div>
-  );
+   return (
+      <div className="App">
+         <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet" />
+         <ProductNote type="note" title="Example Note" html="Example Contents" />
+         <ProductNote type="disclaimer" title="Example Disclaimer" html="Example Contents" />
+         <ProductNote type="warning" title="Example Warning" html="Example Contents" />
+         <hr />
+         <ReviewStars numReviews={30} average={3.2} starSize={18} />
+         <hr />
+         <ProductReviews
+            productReviews={exampleReviewsNone}
+            langid="en"
+            itemcode="IF145-002-4"
+            reviewsLink={reviewsLink}
+         />
+         <hr />
+         <ProductReviews
+            productReviews={exampleReviews}
+            langid="en"
+            itemcode="IF145-002-4"
+            reviewsLink={reviewsLink}
+         />
+         <hr />
+         <RecommenededProductsExample />
+      </div>
+   );
 }
 
-const addToCart = skus => console.log("Add to Cart:", skus);
+const addToCart = skus => console.log('Add to Cart:', skus);
 
 function RecommenededProductsExample() {
    const fewerProducts = {
       ...exampleProducts,
-      relatedProducts: exampleProducts.relatedProducts.slice(0,1)
+      relatedProducts: exampleProducts.relatedProducts.slice(0, 1),
    };
 
    return (
       <>
          <SpacedRecommendedProducts>
-            <RecommendedProducts
-              {...exampleProducts}
-              addToCart={addToCart}
-            />
+            <RecommendedProducts {...exampleProducts} addToCart={addToCart} />
          </SpacedRecommendedProducts>
-         <hr/>
+         <hr />
          <SpacedRecommendedProducts>
-            <RecommendedProducts
-              {...exampleProductsLongTitles}
-              addToCart={addToCart}
-            />
+            <RecommendedProducts {...exampleProductsLongTitles} addToCart={addToCart} />
          </SpacedRecommendedProducts>
-         <hr/>
+         <hr />
          <SpacedRecommendedProducts>
-            <RecommendedProducts
-              {...fewerProducts}
-              addToCart={addToCart}
-            />
+            <RecommendedProducts {...fewerProducts} addToCart={addToCart} />
          </SpacedRecommendedProducts>
-         <hr/>
+         <hr />
          <SpacedRecommendedProducts>
-            <RecommendedProducts
-              {...exampleProductsFloatingPointIssues}
-              addToCart={addToCart}
-            />
+            <RecommendedProducts {...exampleProductsFloatingPointIssues} addToCart={addToCart} />
          </SpacedRecommendedProducts>
       </>
    );
 }
 
-render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById('root'));
