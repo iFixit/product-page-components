bcdc98e7dd042430630bab78a1107fe64fce5356
Zach Mitchell <51836911+zdmitchell@users.noreply.github.com>
2021-03-12 13:52:57 -0800

    Revert "Product Page Components: Fix dist issues"

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,5 +4,4 @@ export { default as ProductNote } from './product_note/product_note.js';
 export { default as RecommendedProducts } from './recommended_products/recommended_products.js';
 export { default as NotifyProduct } from './notify_product/notify_product.js';
 export { default as StoreDropdown } from './dropdowns/store_dropdown/store_dropdown.js';
-export { default as StoreFlag } from './dropdowns/store_dropdown/store_flag';
 export { default as LanguageDropdown } from './dropdowns/language_dropdown/language_dropdown';
